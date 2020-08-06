511a2bb00fc38f03179651f1e0db644e384c2453
sumit <sumit.pradhan65@gmail.com>
2020-08-06 23:54:38 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const socket = require('socket.io')
 const http = require('http')
+const { isObject } = require('util')
 const app = express()
 const server = http.createServer(app)
 const PORT = process.env.PORT || 3000
 
+socket.on('connection', ()=>{
+    socket.broadcast.emit('message', 'A New Client Joined')
+    socket.emit('message', 'You joined')
+})
 
 
 server.listen(PORT, ()=>{
